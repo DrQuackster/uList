49d31df9d212304ad95a1cd9b907c47191996c08
DrQuackster <128331393+DrQuackster@users.noreply.github.com>
2023-12-18 23:06:36 -0300

    Update uScriptCLI.py

--- uScriptCLI.py
+++ uScriptCLI.py
@@ -1,119 +1,69 @@
 import requests
-import json
-import re
+import argparse
 
-FILTERLISTS_API_URL = "http://filterlists.com/api/directory"
+BASE_URL = "http://filterlists.com/api/directory/v1"
 
-def merge_filters(filter_paths):
-    merged_filter = []
+def search_filterlists(api_url, api_key, query, category=None, name=None, software=None, maintainer=None, license=None):
+    endpoint = f"{api_url}/lists"
+    headers = {"Authorization": f"Bearer {api_key}"}
+    params = {"query": query, "category": category, "name": name, "software": software, "maintainer": maintainer, "license": license}
 
-    for path in filter_paths:
-        with open(path, 'r') as filter_file:
-            lines = filter_file.readlines()
-            merged_filter.extend(lines)
+    response = requests.get(endpoint, headers=headers, params=params)
 
-    return merged_filter
-
-def search_filter_lists(command, value=None):
-    url = f"{FILTERLISTS_API_URL}/lists"
-    
-    if command:
-        url += f"?{command}"
-        if value:
-            url += f"={value}"
-
-    response = requests.get(url)
-    filter_lists = response.json()
-
-    return filter_lists
-
-def print_filter_list_details(filter_list):
-    print(f"ID: {filter_list['id']}")
-    print(f"Name: {filter_list['name']}")
-    print(f"Description: {filter_list['description']}")
-    print(f"View URL: {filter_list['viewUrl']}")
-    print("-" * 30)
-
-def list_filters_by_category(command):
-    url = f"{FILTERLISTS_API_URL}/{command}"
-    response = requests.get(url)
-    filters = response.json()
-
-    if filters:
-        print(f"{command.capitalize()} Filters:")
-        for filter_info in filters:
-            print(f"ID: {filter_info['id']}, Name: {filter_info['name']}")
+    if response.status_code == 200:
+        return response.json()
     else:
-        print(f"No {command.capitalize()} filters found.")
+        print(f"Error: {response.status_code}")
+        return None
 
-# CLI code
-while True:
-    print("1. Merge adblock filters")
-    print("2. Search for filters in filterlists.com")
-    print("3. Replace metadata in a filter file")
-    print("4. Validate a filter file")
-    print("5. Exit")
+def get_languages(api_url):
+    endpoint = f"{api_url}/languages"
+    response = requests.get(endpoint)
+    return response.json()
 
-    choice = input("Enter your choice (1-5): ")
+# Add similar functions for other endpoints if needed
 
-    if choice == '1':
-        num_filters = int(input("Enter the number of filters to merge: "))
-        filter_paths = []
-        for i in range(num_filters):
-            path = input(f"Enter the path of filter {i+1}: ")
-            filter_paths.append(path)
+def main():
+    parser = argparse.ArgumentParser(description="Interact with FilterLists Directory API from the command line")
+    subparsers = parser.add_subparsers(dest="command", help="Specify the command")
 
-        merged_filter = merge_filters(filter_paths)
-        print("Merged Filter:")
-        print(''.join(merged_filter))
+    # Search command
+    search_parser = subparsers.add_parser("search", help="Search FilterLists")
+    search_parser.add_argument("query", nargs='?', default=None, help="Search query")
+    search_parser.add_argument("-L", "--languages", action="store_true", help="Include languages in the search results")
+    search_parser.add_argument("-n", "--name", help="Filter by name")
+    search_parser.add_argument("-s", "--software", help="Filter by software")
+    search_parser.add_argument("-m", "--maintainer", help="Filter by maintainer")
+    search_parser.add_argument("-l", "--license", help="Filter by license")
+    search_parser.add_argument("--all", action="store_true", help="Show all details for each result")
 
-    elif choice == '2':
-        command = input("Enter the command to search for filters (/languages, /software, /name, /license, /syntaxes, /maintainers, /tags): ")
-        
-        if command in ['/languages', '/software', '/name', '/license', '/syntaxes', '/maintainers', '/tags']:
-            value = None
-            if command == '/name':
-                value = input("Enter the filter name: ")
-            elif command == '/license':
-                value = input("Enter the license ID: ")
-            elif command == '/syntaxes':
-                value = input("Enter the syntax ID: ")
-            elif command == '/maintainers':
-                value = input("Enter the maintainer ID: ")
-            elif command == '/tags':
-                list_filters_by_category('tags')
-                continue  # Skip the rest of the loop since we've already listed the filters
+    args = parser.parse_args()
 
-            filter_lists = search_filter_lists(command, value)
-            
-            if filter_lists:
-                print("Filter Lists:")
-                for filter_list in filter_lists:
-                    print_filter_list_details(filter_list)
-            else:
-                print("No filter lists found for the given command.")
+    if args.command == "search":
+        result = search_filterlists(
+            BASE_URL,
+            api_key=None,  # Replace with your actual API key
+            query=args.query,
+            category=None,
+            name=args.name,
+            software=args.software,
+            maintainer=args.maintainer,
+            license=args.license
+        )
 
+        if result:
+            print("Search results:")
+            for item in result.get("items", []):
+                details = f"Name: {item.get('name')}, Description: {item.get('description')}"
+                if args.languages:
+                    languages = get_languages(BASE_URL)
+                    details += f", Languages: {', '.join(languages)}"
+                if args.all:
+                    # Include additional details here
+                    pass
+                print(details)
         else:
-            print("Invalid command. Please enter a valid command.")
+            print("Failed to fetch search results. Please check your input.")
 
-    elif choice == '3':
-        filter_path = input("Enter the path of the filter file: ")
-        new_metadata = input("Enter the new metadata: ")
-        replace_metadata(filter_path, new_metadata)
-        print("Metadata replaced successfully!")
-
-    elif choice == '4':
-        filter_path = input("Enter the path of the filter file: ")
-        with open(filter_path, 'r') as filter_file:
-            filter_lines = filter_file.readlines()
-
-        if validate_filter(filter_lines):
-            print("Filter is valid!")
-        else:
-            print("Filter is not valid!")
-
-    elif choice == '5':
-        break
-
-    print("\n")
-print("Exiting the program...")
+if __name__ == "__main__":
+    main()
