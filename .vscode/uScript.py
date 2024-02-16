11d175747669ef25b4bf1ccd963124cc3cd9415b
DrQuackster <128331393+DrQuackster@users.noreply.github.com>
2024-02-16 18:38:25 -0300

    Update uScript.py

--- .vscode/uScript.py
+++ .vscode/uScript.py
@@ -1,144 +1,51 @@
 import os
 import requests
-import urllib.parse
-import re
-import datetime
-import publicsuffixlist
+from git import Repo
 
-psl = publicsuffixlist.PublicSuffixList()
+# GitHub credentials
+github_username = "your_github_username"
+github_token = "your_github_token"
 
-LIST_FILENAME = "list.txt"
-STATUS_FILENAME = "status.txt"
-DOMAIN_FILENAME = "domains.txt"
-OUTPUT_DIRECTORY = r"C:\Users\ASUS\Documents\uList\uList.txt"
+# GitHub repository information
+repo_owner = "your_github_username"
+repo_name = "uList"
 
-lists = {
-    "Dandelion Sprout's Anti-Malware List": "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Dandelion%20Sprout's%20Anti-Malware%20List.txt",
-    "The malicious website blocklist": "https://raw.githubusercontent.com/iam-py-test/my_filters_001/main/Alternative%20list%20formats/antimalware_lite.txt",
-    "iam-py-test's antitypo list": "https://raw.githubusercontent.com/iam-py-test/my_filters_001/main/antitypo.txt",
-    "Actually Legitimate URL Shortener Tool": "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/LegitimateURLShortener.txt",
-    "HaGeZi ULTIMATE": "https://gitlab.com/hagezi/mirror/-/raw/main/dns-blocklists/adblock/tif.txt"
-}
+# List of uBO filter lists to merge
+filter_lists = [
+    "https://example.com/filterlist1.txt",
+    "https://example.com/filterlist2.txt",
+    # Add more filter lists as needed
+]
 
-donelines = []
-donedomains = []
-excludes = requests.get("https://raw.githubusercontent.com/iam-py-test/allowlist/main/filter.txt").text.split("\n")
-subdomains = requests.get("https://raw.githubusercontent.com/iam-py-test/tracker_analytics/main/kdl.txt").text.split("\n")
-subdomains += requests.get("https://raw.githubusercontent.com/iam-py-test/my_filters_001/main/Alternative%20list%20formats/antimalware_domains.txt").text.split("\n")
-subdomains += requests.get("https://raw.githubusercontent.com/iam-py-test/cloudflare-usage/main/cnames.txt").text.split("\n")
+# Clone the uList repository
+repo_url = f"https://github.com/{repo_owner}/{repo_name}.git"
+local_repo_path = f"./{repo_name}"
 
-is_ip_v4 = r"^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
-is_ip_v6 = "((([0-9a-fA-F]){1,4})\\:){7}"\
-             "([0-9a-fA-F]){1,4}"
-is_ip_v4_reg = re.compile(is_ip_v4)
-is_ip_v6_reg = re.compile(is_ip_v6)
+# Clone the repository if it doesn't exist, otherwise pull the latest changes
+if not os.path.exists(local_repo_path):
+    Repo.clone_from(repo_url, local_repo_path)
+else:
+    repo = Repo(local_repo_path)
+    origin = repo.remote(name='origin')
+    origin.pull()
 
+# Fetch and merge filter lists
+merged_content = ""
+for filter_list_url in filter_lists:
+    response = requests.get(filter_list_url)
+    if response.status_code == 200:
+        merged_content += f"\n\n# {filter_list_url}\n\n" + response.text
 
-def isipdomain(domain):
-    if re.search(is_ip_v4_reg, domain):
-        return True
-    if re.search(is_ip_v6_reg, domain):
-        return True
-    return False
+# Write merged content to the uList file
+ulist_file_path = os.path.join(local_repo_path, "uList.txt")
+with open(ulist_file_path, "a") as ulist_file:
+    ulist_file.write(merged_content)
 
+# Stage and commit changes using Git command line
+os.system(f'git -C "{local_repo_path}" add uList.txt')
+os.system(f'git -C "{local_repo_path}" commit -m "Merge uBO filter lists"')
 
-def extdomain(line):
-    try:
-        domain = ""
-        if line.startswith("||") and line.endswith("^$all"):
-            domain = line[2:-5]
-        # ... (rest of the extdomain function)
+# Push changes to GitHub using Git command line
+os.system(f'git -C "{local_repo_path}" push origin master')
 
-        return domain
-    except:
-        return ""
-
-
-mainlist_header = """! Title: uList
-! Description: The useless List
-! Script last updated: 22/11/2023
-! Last updated: {}
-! Homepage: https://github.com/DrQuackster/uList
-! License: https://github.com/DrQuackster/uList/blob/master/LICENSE.md
-! Syntax: Adblock Plus Filter List
-! Maintainer: Dr. Duckenstein
-! Contact: drduckenstein@protonmail.com
-
-""".format(datetime.date.today().strftime("%d/%m/%Y"))
-
-eadd = 0
-ered = 0
-parselist = None # type: ignore
-
-
-def parselist(lines, curl=""):
-    global donedomains
-    global donelines
-    global eadd
-    global ered
-    plist = ""
-    for line in lines:
-        if (line.startswith("!") or line.startswith("#")) and "include" not in line:
-            continue
-        elif line.startswith("[Adblock") and line.endswith("]"):
-            continue
-        elif line in donelines:
-            ered += 1
-        elif line in excludes:
-            continue
-        elif line == "":
-            continue
-        elif extdomain(line) != "" and extdomain(line) in donedomains:
-            continue
-        elif line.startswith("!#include "):
-            try:
-                incpath = urllib.parse.urljoin(curl, line[10:], allow_fragments=True)
-                inccontents = requests.get(incpath).text.replace("\r", "").split("\n")
-                endcontents = parselist(inccontents, incpath)
-                plist += "{}\n".format(endcontents)
-            except Exception as err:
-                print(line, err)
-        else:
-            plist += "{}\n".format(line)
-            eadd += 1
-            donelines.append(line)
-            edomain = extdomain(line)
-            if edomain != "" and edomain != " ":
-                donedomains.append(edomain)
-    return plist
-
-
-for clist in lists:
-    list_url = lists[clist]
-    list_content = requests.get(list_url).text.split("\n")
-    mainlist_header += parselist(list_content, list_url)
-
-with open(OUTPUT_DIRECTORY, "w", encoding="UTF-8") as f:
-    f.write(mainlist_header)
-
-justdomains = [d for d in donedomains if "/" not in d and "." in d and "*" not in d and d != "" and not d.endswith(".") and not isipdomain(d)]
-
-with open(DOMAIN_FILENAME, "w", encoding="UTF-8") as f:
-    f.write("\n".join(justdomains))
-
-subsfound = 0
-domainplussub = justdomains
-for sub in subdomains:
-    try:
-        maindomain = psl.privatesuffix(sub)
-        if maindomain in domainplussub and sub not in domainplussub:
-            subsfound += 1
-            domainplussub.append(sub)
-    except Exception as err:
-        print(err, sub)
-
-with open("domains_subdomains.txt", "w", encoding="UTF-8") as f:
-    f.write("\n".join(justdomains))
-
-with open(STATUS_FILENAME, 'w') as status:
-    status.write("""Stats:
-{} entries added
-{} redundant entries removed
-{} domains added
-{} subdomains added
-""".format(eadd, ered, len(donedomains), subsfound))
+print("Filter lists merged and committed successfully.")
